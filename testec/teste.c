2a51a121278fe58b2df4685d52f8b494ec0333b6
Marcelo Cohen <mflashbr@gmail.com>
2026-03-10 14:05:57 +0000

    Exemplo de printf e scanf

--- testec/teste.c
+++ testec/teste.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
+#include <math.h>
 
 int main()
 {
+    int a = 5;
     printf("Olá pessoal\n");
+    printf("Valor de a: %d\n", a);
+    printf("Digite o valor de b: ");
+    int b;
+    scanf("%d", &b);
+    printf("O valor de b é: %d\n", b);
+    printf("O seno de 90 graus é: %f\n", sin(M_PI/2));
 }
